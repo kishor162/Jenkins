f0ccf4e8888cadc722d09c08d41de6d1147e1aa1
ME <ME@LAPTOP-DS4ESSB9.mshome.net>
2022-05-30 15:45:36 +0530

    Basic class

---
+++ src/test/java/BasicProgramTest/Program2Test.java
@@ -0,0 +1,15 @@
+package BasicProgramTest;
+
+import org.testng.annotations.Test;
+
+public class Program2Test {
+	
+	@Test
+	public void empolyesname() {
+		System.out.println("Shrikant");
+		System.out.println("JayPrakash");
+		System.out.println("chetna");
+		System.out.println("Priya");
+	}
+
+}
---
+++ src/test/java/BasicProgramTest/Program3Test.java
@@ -0,0 +1,5 @@
+package BasicProgramTest;
+
+public class Program3Test {
+
+}
